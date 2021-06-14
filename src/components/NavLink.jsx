ee1fb545d0f19f601a78e80bd40680c024804168
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2021-06-14 20:08:10 +0200

    Poprawiłem navbar

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,11 +1,11 @@
 import React from "react"
-import styles from "../styles/NavLink.module.css"
+import * as styles from "../styles/NavLink.module.css"
 
 export default function NavLink({name, link})
 {
     return(
-        <li>
-            <a href={link}>
+        <li className={styles.main}>
+            <a className={styles.a} href={link}>
                 <h3>{name}</h3>
             </a>
         </li>
