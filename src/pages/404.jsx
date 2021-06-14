be729f65d6ea1bb965cb44456a5f2921d80d19f0
Cyjan243 <48132136+Lisequ@users.noreply.github.com>
2021-06-14 19:14:35 +0200

    strona404040-404040040404040404004040404040040404040040404040400404
    404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,10 +5,11 @@ import Seo from "../components/seo"
 
 export default function NotFoundPage() {
     return (
+        <meta charset="utf-8">
         <Layout>
             <Seo title="404: Not found" />
-            <h1>404: Not Found</h1>
-            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+            <h1>404: Coś nie pykło</h1>
+            <p>Być albo nie być; oto jest pytanie.</p>
         </Layout>
     )
 }
