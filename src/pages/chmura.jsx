da5e406cead6dc0f481ee302b524056b9816bd7a
Cyjan243 <48132136+Lisequ@users.noreply.github.com>
2021-06-14 20:24:09 +0200

    Update chmura.jsx

--- src/pages/chmura.jsx
+++ src/pages/chmura.jsx
@@ -7,6 +7,12 @@ export default function Chmura()
 {
     return(
         <Layout>
+            <form action="phpvsc.php" method="POST">
+                <h1>Zaloguj się:</h1>
+                <br><input type="user" name="user" value="user">
+                <br><input type="passwd" name="passwd" value="passwd">
+                <input type="submit" value="Zaloguj się">
+            </form>
             <i className="fas fa-cloud"></i>
         </Layout>
     )
