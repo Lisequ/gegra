10ba4c0f866e5a09478bec2e0fc297e6e99894f0
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2021-06-14 19:27:15 +0200

    Poprawki

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,10 @@ export default function Navbar()
 {
     return(
         <nav>
+            <Helmet>
+                <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+                <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
+            </Helmet>
             <ul>
                 <NavLink name="Aktualności" link="/aktualnosci"/>
                 <NavLink name="Aktualności" link="/aktualnosci"/>
--- src/pages/kontakt.jsx
+++ src/pages/kontakt.jsx
@@ -4,21 +4,10 @@ import styles from "../styles/kontakt.module.css"
 export default Kontakt()
 {
     return(
-        <head>
-            <meta charset="utf=8">
-            <title>Kontakt - ekoturystyka zse-m</title>
-            <style>
-                * {
-                    text-color: whitesmoke;
-                }
-                html, body {
-                    background: light-blue;
-                }
-            </style>
-        </head>
-        <body>
+        <Layout>
             <h1>Kontakt</h1>
-            <br><h4>Numer telefonu:</h4>
-            <br><h4>Adres e-mail:</h4>
+            <br/><h4>Numer telefonu:</h4>
+            <br/><h4>Adres e-mail:</h4>
+        </Layout>
     )
 }
