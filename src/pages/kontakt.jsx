7470be0563c5b9272e2e7f86507f9ec93baecd2f
Cyjan243 <48132136+Lisequ@users.noreply.github.com>
2021-06-14 19:21:39 +0200

    kontaktV1

--- src/pages/kontakt.jsx
+++ src/pages/kontakt.jsx
@@ -4,6 +4,21 @@ import styles from "../styles/kontakt.module.css"
 export default Kontakt()
 {
     return(
-        <h1>kontakt</h1>
+        <head>
+            <meta charset="utf=8">
+            <title>Kontakt - ekoturystyka zse-m</title>
+            <style>
+                * {
+                    text-color: whitesmoke;
+                }
+                html, body {
+                    background: light-blue;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>Kontakt</h1>
+            <br><h4>Numer telefonu:</h4>
+            <br><h4>Adres e-mail:</h4>
     )
 }
