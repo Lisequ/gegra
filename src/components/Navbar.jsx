b45ed4dba913ef920dd679cdf438199667226708
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2021-06-14 20:05:14 +0200

    Poprawki znowu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import styles from "../styles/Navbar.module.css"
+import * as styles from "../styles/Navbar.module.css"
 
 import { Helmet } from "react-helmet"
 import NavLink from "./NavLink"
@@ -7,16 +7,17 @@ import NavLink from "./NavLink"
 export default function Navbar()
 {
     return(
-        <nav>
+        <nav className={styles.nav}>
             <Helmet>
                 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
                 <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
             </Helmet>
-            <ul>
+            <ul className={styles.list}>
                 <NavLink name="Aktualności" link="/aktualnosci"/>
-                <NavLink name="Aktualności" link="/aktualnosci"/>
-                <NavLink name="Aktualności" link="/aktualnosci"/>
+                <NavLink name="Chmura" link="/chmura"/>
+                <NavLink name="Kontakt" link="/kontakt"/>
+                <NavLink name="Zaloguj" link="/zaloguj"/>
             </ul>
         </nav>
     )
